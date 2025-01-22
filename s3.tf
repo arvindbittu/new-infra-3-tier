98b58fda4882b103686e8a24d1ec3e6361a024e7
arvindbittu <arvindbittu2324@gmail.com>
2025-01-22 20:16:55 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,6 +1,6 @@
 # Create an S3 Bucket
 resource "aws_s3_bucket" "example_bucket" {
-  bucket = "arvind_terraform_s3_bucket"
+  bucket = "devopsbyraham_s3_bucket"
 
   # Enable versioning
   versioning {
